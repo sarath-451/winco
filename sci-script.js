58f4badb780fd7d429814d5718d8978afc4c173c
sarath-451 <sarath.sarigama@gmail.com>
2020-04-02 21:24:26 +0530

    Create sci-script.js

---
+++ sci-script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var x = document.createElement("STYLE");
+  var t = document.createTextNode("body {font: 0px verdana; color:#000000;}");
+  x.appendChild(t);
+  document.head.appendChild(x);
+  }
