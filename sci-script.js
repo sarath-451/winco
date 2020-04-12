dcffcaa12b6a1354fa9bad2c2c667ad1cf9bbb57
sarath-451 <sarath.sarigama@gmail.com>
2020-04-12 23:05:51 +0530

    Update sci-script.js

--- sci-script.js
+++ sci-script.js
@@ -1,6 +1,4 @@
-function myFunction() {
   var x = document.createElement("STYLE");
   var t = document.createTextNode("body {font: 0px verdana !important; color:#000000 !important;}");
   x.appendChild(t);
   document.head.appendChild(x);
-  }
